6d96c0ae06a4df448dd1e4820a130fe46fff7d96
Shaurya <60067200+shauryasharma30@users.noreply.github.com>
2020-08-07 12:19:49 +0530

    commit

---
+++ Sorting.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 19:29:26 2020
+
+@author: shaurya
+"""
+
+
+def bubble(a):
+    n = len(a)
+    
+    for i in range(n):
+        for j in range(0,n-1):
+            if(a[j]>a[j+1]):
+                tmp = a[j]
+                a[j] = a[j+1]
+                a[j+1]=tmp
+
+
+a = [7,6,3,4,2,1]
+bubble(a)
+
+for i in a:
+    print(i)
+             
