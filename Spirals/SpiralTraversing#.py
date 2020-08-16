ebd79a702f45d9eb354159be6cc5f23a9cf54552
Shaurya <60067200+shauryasharma30@users.noreply.github.com>
2020-08-16 21:44:55 +0530

    commit

---
+++ Spirals/SpiralTraversing#.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 22:14:59 2020
+
+@author: shaurya
+"""
+
+
+import turtle
+
+tur = turtle.Turtle()
+
+for i in range(50):
+    tur.forward(50)
+    tur.right(144)
+    
+turtle.done()    
